9a92a481c47444ee08ef0c492c368c8ab0f5adf2
Yonara-ferreira <yonara.fd@gmail.com>
2023-10-09 09:58:00 -0300

    Main method of service

--- src/main/java/com/example/Climatico/controller/PrevisaoTempoController.java
+++ src/main/java/com/example/Climatico/controller/PrevisaoTempoController.java
@@ -1,7 +1,8 @@
 package com.example.Climatico.controller;
 
 import com.example.Climatico.model.DadosPrincipais;
-import org.springframework.http.ResponseEntity;
+import com.example.Climatico.service.PrevisaoTempoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/previsao")
 public class PrevisaoTempoController {
 
+    @Autowired
+    private PrevisaoTempoService service;
+
     @GetMapping("/{cidade}")
-    public ResponseEntity<DadosPrincipais>obterPrevisaoTempoPorCidade(@PathVariable String cidade){
-        return ResponseEntity.ok();
+    public DadosPrincipais obterPrevisaoTempoAtual(@PathVariable String cidade) {
+        return service.previsaoDoTempo(cidade);
     }
 
 }
--- src/main/java/com/example/Climatico/service/PrevisaoTempoService.java
+++ src/main/java/com/example/Climatico/service/PrevisaoTempoService.java
@@ -1,14 +1,20 @@
-package com.example.Climatico;
+package com.example.Climatico.service;
 
+import com.example.Climatico.model.DadosPrincipais;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
-public class previsaoTempoService extends RuntimeException {
+public class PrevisaoTempoService extends RuntimeException {
 
     @Autowired
     private RestTemplate template = new RestTemplate();
 
+    public DadosPrincipais previsaoDoTempo(String cidade){
+        DadosPrincipais previsaoTempo = template.getForObject("https://api.openweathermap.org/data/2.5/weather?q="+ cidade +"&appid=2ad90ed5fc16cde0674b8e2be9186c80&lang=pt_br", DadosPrincipais.class);
+
+        return previsaoTempo;
+    }
 
 }
