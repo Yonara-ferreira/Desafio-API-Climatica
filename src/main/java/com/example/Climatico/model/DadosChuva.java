00faf03a997b82f1f77f169f2c4d03b97d65c28f
Yonara-ferreira <yonara.fd@gmail.com>
2023-10-13 17:43:35 -0300

    Restructuring all models

--- src/main/java/com/example/Climatico/model/DadosChuva.java
+++
@@ -1,8 +0,0 @@
-package com.example.Climatico.model;
-
-import com.fasterxml.jackson.annotation.JsonAlias;
-
-public class DadosChuva {
-    @JsonAlias("3h")
-    private double _3h;
-}
--- src/main/java/com/example/Climatico/model/DadosNuvens.java
+++
@@ -1,8 +0,0 @@
-package com.example.Climatico.model;
-
-import com.fasterxml.jackson.annotation.JsonAlias;
-
-public class DadosNuvens {
-    @JsonAlias("all")
-    private int tudo;
-}
